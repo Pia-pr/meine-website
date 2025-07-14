20b926db1a34d77a85906b736e5d611dd37714b6
Pia-pr <piaraikowski@gamil.com>
2025-07-14 13:33:53 +0200

    Versuch nummer 4 mit render datenbank

--- db.js
+++ db.js
@@ -6,28 +6,52 @@ const pool = new Pool({
   ssl: { rejectUnauthorized: false }, // wichtig für Render
 });
 
+// Alle Benutzer auslesen
 export async function getUsers() {
-  const res = await pool.query('SELECT * FROM users');
-  return res.rows;
+  try {
+    const res = await pool.query('SELECT * FROM users');
+    return res.rows;
+  } catch (err) {
+    console.error('Fehler bei getUsers:', err);
+    throw err;
+  }
 }
 
+// Benutzer nach Benutzernamen auslesen
 export async function getUserByUsername(username) {
-  const res = await pool.query('SELECT * FROM users WHERE benutzername = $1', [username]);
-  return res.rows[0];
+  try {
+    const res = await pool.query('SELECT * FROM users WHERE benutzername = $1', [username]);
+    return res.rows[0];
+  } catch (err) {
+    console.error('Fehler bei getUserByUsername:', err);
+    throw err;
+  }
 }
 
+// Neuen Benutzer hinzufügen
 export async function addUser(benutzername, passwort) {
-  await pool.query(
-    'INSERT INTO users (benutzername, passwort, login_history) VALUES ($1, $2, $3)',
-    [benutzername, passwort, []]
-  );
+  try {
+    await pool.query(
+      'INSERT INTO users (benutzername, passwort, login_history) VALUES ($1, $2, $3)',
+      [benutzername, passwort, []]
+    );
+  } catch (err) {
+    console.error('Fehler bei addUser:', err);
+    throw err;
+  }
 }
 
+// Login-Zeit zur Login-History hinzufügen
 export async function updateLastLogin(username, time) {
-  await pool.query(
-    `UPDATE users
-     SET login_history = array_append(login_history, $2)
-     WHERE benutzername = $1`,
-    [username, time]
-  );
+  try {
+    await pool.query(
+      `UPDATE users
+       SET login_history = array_append(login_history, $2)
+       WHERE benutzername = $1`,
+      [username, time]
+    );
+  } catch (err) {
+    console.error('Fehler bei updateLastLogin:', err);
+    throw err;
+  }
 }
